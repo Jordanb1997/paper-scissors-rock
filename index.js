326b3718d1a0b28a30b9fdab66818adad8cd59d8
jordan <jordanb1997@gmail.com>
2023-04-26 09:37:29 +1000

    refactored playRound

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+//choices
 let playerChoice;
 let computerChoice;
-let playerScore;
-let computerScore;
+//score
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
+let gameWinner = "";
 
 function getComputerChoice() {
     let rng = Math.floor(Math.random() * 3);
-    let choice;
 
     switch (rng) {
         case 0:
@@ -14,50 +17,28 @@ function getComputerChoice() {
             return "scissors"
         case 2:
             return "rock"
-
     }
 }
 
 function playRound(playerChoice, computerChoice) {
-    let result = ""
+    if (playerChoice === computerChoice) {
+        roundWinner = "It's a draw";
+    } if (playerChoice === "paper" && computerChoice === "rock" ||
+        playerChoice === "scissors" && computerChoice === "paper" ||
+        playerChoice === "rock" && computerChoice === "scissors") {
 
-    if (playerChoice === 'paper') {
-        if (computerChoice === "paper") {
-            result = "It's a draw!";
+        roundWinner = "Player wins!";
+        playerScore++;
 
-        } else if (computerChoice === 'scissors') {
-            result = "You lose!, Scissors beats Paper";
-        } else if (computerChoice === "rock") {
-            result = "You win!, Paper beats Rock";
-        }
+    } if (playerChoice === "paper" && computerChoice === "scissors" ||
+        playerChoice === "scissors" && computerChoice === "rock" ||
+        playerChoice === "rock" && computerChoice === "paper") {
+
+        roundWinner = "Computer wins!"
+        computerScore++;
     }
-    if (playerChoice === "scissors") {
-        if (computerChoice === "paper") {
-            result = "You win!, Scissors beats Paper";
-        } else if (computerChoice === 'scissors') {
-            result = "It's a draw!";
-        } else if (computerChoice === "rock") {
-            result = "You lose!, Rock beats Scissors";
-        }
-    }
-    if (playerChoice === "rock") {
-        if (computerChoice === "paper") {
-            result = "You lose!, Paper beats Rock";
-        } else if (computerChoice === 'scissors') {
-            result = "You win!, Rock beats Scissors";
-        } else if (computerChoice === "rock") {
-            result = "It's a draw!";
-        }
-    }
-    playerChoice = null;
-    return result
 }
 
 function game() {
-    let count = 0;
-
-    if (count < 5) {
-
-    }
 
 }
